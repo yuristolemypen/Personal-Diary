bf0527c192f74266dc06756fc9d11699b18a435f
aio <aiminsupremacy@gmail.com>
2025-11-10 22:58:19 +0800

    Finished the initial UI of the 4 tabs

--- scripts/index-main/entryInfo2.js
+++ scripts/index-main/entryInfo2.js
@@ -10,25 +10,25 @@ export function renderEntryInfo2() {
 
   entryInfo2.forEach((info) => {
     entryInfoHTML += `
-    <article class="entry-grid-container">
-      <aside class="entry-info">
-        <div class="entry-profile-container">
-          <img class="entry-profile" src="${info.entryProfile}">
+      <article class="entry-grid-container">
+        <aside class="entry-info">
+          <div class="entry-profile-container">
+            <img class="entry-profile" src="${info.entryProfile}">
+          </div>
+          <div class="entry-name-container">
+            <p class="entry-name">${info.entryName}</p>
+          </div>
+          <div class="date-and-time-container">
+            <p>${info.entryDate}</p>
+            <p>${info.entryTime}</p>
+          </div>
+        </aside>
+        <div class="entry-content">
+          <div class="entry-scroll">
+            <p>${info.entryInfo}</p>
+          </div>
         </div>
-        <div class="entry-name-container">
-          <p class="entry-name">${info.entryName}</p>
-        </div>
-        <div class="date-and-time-container">
-          <p>${info.entryDate}</p>
-          <p>${info.entryTime}</p>
-        </div>
-      </aside>
-      <div class="entry-content">
-        <div class="entry-scroll">
-          <p>${info.entryInfo}</p>
-        </div>
-      </div>
-    </article>
+      </article>
     `
   });
 
